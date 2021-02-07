b1065c3049cecb73d2d22d25f13525074a8c996c
Julius Canute <juliuscanute@gmail.com>
2021-02-08 08:03:45 +1100

    install beacon formula

---
+++ jbeacon.rb
@@ -0,0 +1,10 @@
+class JBeacon < Formula
+    desc "Broadcast Beacon from your Mac"
+    homepage ""
+    url "https://github.com/juliuscanute/jbeacon/releases/download/v1.0.0/jbeacon.zip"
+    sha256 "42b8e59579ab980032ccdf6d3617bdcf2426b3d6a58e1426af47f891f5768e52"
+  
+    def install
+      bin.install 'jbeacon'
+    end
+  end
