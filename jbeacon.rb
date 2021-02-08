74a1cede5b379e421230ef6d4bd4f506e7e2c570
juliuscanute <juliuscanute@gmail.com>
2021-02-09 09:07:27 +1100

    Update jbeacon.rb

--- jbeacon.rb
+++ jbeacon.rb
@@ -1,8 +1,8 @@
 class Jbeacon < Formula
     desc "Broadcast Beacon from your Mac"
     homepage ""
-    url "https://github.com/juliuscanute/jbeacon/releases/download/v1.0.0/jbeacon.zip"
-    sha256 "42b8e59579ab980032ccdf6d3617bdcf2426b3d6a58e1426af47f891f5768e52"
+    url "https://github.com/juliuscanute/jbeacon/releases/download/v1.1.1/jbeacon.zip"
+    sha256 "a4d58ca20a41186d7c218bbac0e7b7d9e4bda5ab56b757494bae5e21aa40b9f2"
   
     def install
       bin.install 'jbeacon'
