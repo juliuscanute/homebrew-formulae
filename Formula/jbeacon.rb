037383a6ad268f53ad830d325f09edcb388fe425
juliuscanute <juliuscanute@gmail.com>
2021-02-13 18:18:50 +1100

    Bump up jbeacon formula to v1.2.0 version

--- Formula/jbeacon.rb
+++ Formula/jbeacon.rb
@@ -1,8 +1,8 @@
 class Jbeacon < Formula
     desc "Broadcast Beacon from your Mac"
     homepage ""
-    url "https://github.com/juliuscanute/jbeacon/releases/download/v1.1.10/jbeacon.zip"
-    sha256 "a1a6fe5f5666f85d10ec33aebb807a7863d2ad7b04bb6f99bf3b0f22315f078c"
+    url "https://github.com/juliuscanute/jbeacon/releases/download/v1.2.0/jbeacon.zip"
+    sha256 "5782d39c17dbe6ea83bef5bb1f119488d204e9a795301130d7390a278271a7d2"
   
     def install
       bin.install 'jbeacon'
